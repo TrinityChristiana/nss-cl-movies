2a242c735d085e486ec882926200e371b43322e4
Trinity Terry <trinitycterry@gmail.com>
2020-01-23 11:36:29 -0600

    added function

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+const getMovies = () => {
+    const movieURL = "http://localhost:8088/movies";
+
+    fetch(movieURL)
+        .then(response => response.json())
+        .then(movies => renderMovies(movies));
+};
+
+
+//  Function that argument (list of movies) displays each item on console.log
+const renderMovies = (movies) => {
+    const movieContainer = document.querySelector("#movie-container")
+    movies.forEach(movie => {
+        movieContainer.innerHTML += movieFactory(movie);
+    });
+};
+
+// function that create html for each movie array item
+const movieFactory = (movie) => {
+    return `
+    <section class="movie">
+        <h2>Title: ${movie.title}</h2>
+        <h2>Date: ${movie.yearReleased}</h2>
+    </section>
+    `;
+};
+
+getMovies();
